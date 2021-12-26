d9fc4f949b9d615b43361aa25c06f65af43dddc6
Ajinkya Pasalkar <ajinkyaphy@gmail.com>
2021-12-26 21:34:00 +0530

    Add Servo motor code

---
+++ aa_servo.ino
@@ -0,0 +1,23 @@
+/*!
+    @brief    Initialise Servo motor
+*/
+void servo_init(void)
+{
+  Servo1.attach(PIN_SERVO);
+  Servo1.write(0);
+}
+
+/*!
+    @brief    Set Servo motor angle
+*/
+void servo_set_angle(uint8_t angle)
+{
+  static uint8_t currAngle = 0;
+
+  if (currAngle != angle)
+  {
+    currAngle = angle;
+    Servo1.write(currAngle);
+  }
+  
+}
--- annealer.ino
+++ annealer.ino
@@ -2,6 +2,7 @@
 #include "Adafruit_ILI9341.h"
 #include <Fonts/FreeSerifBold12pt7b.h>
 #include <Fonts/FreeSerifBold18pt7b.h>
+#include <Servo.h>
 
 
 #include "PIN.h"
@@ -9,9 +10,11 @@
 
 // Global Initialization
 Adafruit_ILI9341 tft = Adafruit_ILI9341(PIN_TFT_CS, PIN_TFT_DC);
+Servo Servo1;
 
 void setup()
 {
+  servo_init();
   ed_init();
   tft_init();
 }
